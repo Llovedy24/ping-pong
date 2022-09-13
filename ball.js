605dc86ad1a995f73d72113442cf74c92d6d6da9
Lovedy Louis-Jean <lovedylouisjean@LovedysacStudio.lan1>
2022-09-12 21:25:20 -0400

    second commit

--- ball.js
+++ ball.js
@@ -34,4 +34,30 @@ expert default class Ball {
         }
         this.velocity = INITIAL_VELOCITY
     }
+    update(delta, paddleRects) {
+        this.x += this.direction.x * this.velocity * delta
+        this.y += this.direction.y * this.velocity * delta
+        this.velocity += VELOCITY_INCREASE * delta
+        const rect = this.rect()
+
+        if (rect.bottom >= window.innerHeight || rect.top <= 0) {
+            this.direction.x *= -1
+        }
+
+        if (paddleRects.some(r => isCollision(r, rect))){
+            this.direction.x *= -1
+        }
+    }
+}
+function randomNumberBetween(min, max) {
+    return Math.random() * (max - min) + min
+}
+
+function isCollision(rect1, rect2 {
+    return (
+        rect1.left <= rect2.right &&
+        rect1.right >= rect2.left &&
+        rect1.top <= rect2.bottom &&
+        rect1.bottom >= rect2.top
+    )
 }
